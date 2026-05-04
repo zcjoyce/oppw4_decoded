82e5bc5ab866f62c7230e8ee8b1281490304039a
zcjoyce <zcjoyce1994@hotmail.com>
2026-05-04 16:50:49 +0200

    Add decoded OPPW4 text files

---
+++ RECOMBINE_LARGE_TEXT_FILES.ps1
@@ -0,0 +1,22 @@
+﻿param([string]$Root = ".")
+
+Get-ChildItem -Path $Root -Directory -Recurse -Filter "*.parts" | ForEach-Object {
+    $TargetFile = $_.FullName.Substring(0, $_.FullName.Length - 6)
+
+    $Writer = New-Object System.IO.StreamWriter($TargetFile, $false, [System.Text.Encoding]::UTF8)
+
+    try {
+        Get-ChildItem -Path $_.FullName -File |
+            Sort-Object Name |
+            ForEach-Object {
+                Get-Content $_.FullName | ForEach-Object {
+                    $Writer.WriteLine($_)
+                }
+            }
+    }
+    finally {
+        $Writer.Dispose()
+    }
+
+    Write-Host "Rebuilt $TargetFile"
+}
